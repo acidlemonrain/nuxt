9ad0907b8ab526256ead365af44b3aa5cd656100
hyc <992273994@qq.com>
2019-07-02 19:18:27 +0800

    second

---
+++ pages/about/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="alert alert-primary" role="alert">A simple primary alert—check it out!</div>
+    <div class="alert alert-secondary" role="alert">A simple secondary alert—check it out!</div>
+    <div class="alert alert-success" role="alert">A simple success alert—check it out!</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,17 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        linux
-      </h1>
-      <h2 class="subtitle">
-        mylinux
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <h1>Lorem ipsum dolor sit amet consectetur.</h1>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
 
 export default {
   components: {
     Logo
   }
-}
+};
 </script>
 
 <style>
@@ -49,8 +25,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
